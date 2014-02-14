af09808aeb555dadeb6cf616b2b54771efdbb8ed
Jamon Holmgren <jamon@clearsightstudio.com>
2014-02-13 19:46:40 -0800

    Added view hierarchy, get, delete, context

--- lib/motion-kit/layout/layout.rb
+++ lib/motion-kit/layout/layout.rb
@@ -1,42 +1,69 @@
 module MotionKit
   class Layout
 
-    def view
-      @view ||= build_layout
+    def initialize
+      @view_hierarchy ||= [ self.view ]
+      layout
     end
 
-    def add(elem, stylename)
-      @view_hierarchy ||= [ self.view ]
-      elem = initialize_view(elem)
-      self.current_view.addSubview(elem)
+    # The parent view.
+    def view
+      @view ||= UIView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    end
+
+    # Adds a view and allows for subviews or styling within the optional block.
+    def add(element, element_id)
+      # Initialize the element, if necessary
+      element = initialize_view(element)
+
+      # Set the name of the element
+      self.element_ids[element_id] = WeakRef.new(element)
+
+      # Add it to the current context
+      self.current_view.addSubview(element)
+
+      # Make the element the new context
       if block_given?
-        @view_hierarchy << elem
+        @view_hierarchy << element
         yield
         @view_hierarchy.pop
       end
+
+      element
     end
 
-    private
+    # Retrieves a view by its element id.
+    def get(element_id)
+      self.element_ids[element_id]
+    end
+
+    # Removes a view from its hierarchy and forgets it entirely.
+    def remove(element_id)
+      elem = self.element_ids[element_id]
+      if elem
+        elem.removeFromSuperview
+        self.element_ids[element_id] = nil
+      end
+      nil
+    end
+
+  protected
+
+    def element_ids
+      @element_ids ||= {}
+    end
 
     def current_view
       @view_hierarchy.last
-    end
-
-    def build_layout
-      view = UIView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
-
-      if respond_to?(:layout)
-        layout
-      end
     end
 
     # Initializes an instance of a view. This will need
     # to be smarter going forward as `new` isn't always
     # the designated initializer.
     def initialize_view(elem)
-       elem = elem.new if elem.respond_to?(:new)
-       elem
-     end
+      elem = elem.new if elem.respond_to?(:new)
+      elem
+    end
  
   end
 end
--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -1,7 +1,29 @@
 describe MotionKit::Layout do
-  before { @subject = TestLayout.new }
+  before { @subject = TestLayout.new; @subject.layout }
 
   it "should be an instance of MotionKit::Layout" do
     @subject.should.be.kind_of(MotionKit::Layout)
+  end
+
+  it "should add a UIView subview with the name :basic_view" do
+    @subject.view.subviews.first.should.be.kind_of UIView
+  end
+
+  it "should add two subviews under :basic_view" do
+    @subject.view.subviews.first.subviews.length.should == 2
+  end
+
+  it "should add a UIButton as the first subview under :basic_view" do
+    @subject.view.subviews.first.subviews.first.should.be.kind_of UIButton
+  end
+
+  it "should add a UILabel as the second subview under :basic_view" do
+    @subject.view.subviews.first.subviews[1].should.be.kind_of UILabel
+  end
+
+  it "should allow getting the subviews by their id" do 
+    @subject.get(:basic_view).should.be.kind_of UIView
+    @subject.get(:basic_button).should.be.kind_of UIButton
+    @subject.get(:basic_label).should.be.kind_of UILabel
   end
 end
