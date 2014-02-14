5734027c74b96a9f166ca40d7d50bf254accf3a7
Colin T.A. Gray <colinta@gmail.com>
2014-02-13 19:58:25 -0700

    quick hide the evidence

--- app/ios/app_delegate.rb
+++ app/ios/app_delegate.rb
@@ -1,28 +1,5 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    p Foo.style
-    p Foo.style.login_button
     true
   end
-end
-
-
-class Styler
-
-  def method_missing(method_name, *args)
-    NSLog('styling %@', method_name)
-  end
-
-end
-
-
-class Foo
-  def self.style
-    @style ||= Styler.new
-  end
-
-  def style.login_button
-    'hi!'
-  end
-
 end
