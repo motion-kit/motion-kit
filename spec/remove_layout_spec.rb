0e1e827eab2d1362871a98e9a08557f80ca255af
Jamon Holmgren <jamon@clearsightstudio.com>
2014-02-19 08:12:49 -0800

    Fixed bug with remove, added specs

---
+++ spec/remove_layout_spec.rb
@@ -0,0 +1,27 @@
+describe MotionKit::Layout do
+  before do
+    @subject = TestRemoveLayout.new
+    @subject.view # initialize it
+    @subject
+  end
+
+  it 'should remove the #label view' do
+    @subject.get(:label).should.be.kind_of(UILabel)
+    @subject.remove_label
+    @subject.get(:label).should.be.nil
+  end
+
+  it 'should remove the #image view' do
+    @subject.get(:image).should.be.kind_of(UIImageView)
+    @subject.remove_image
+    @subject.get(:image).should.be.nil
+  end
+
+  it 'should remove the #view view' do
+    @subject.get(:view).should.be.kind_of(UIView)
+    @subject.remove_view
+    @subject.get(:view).should.be.nil
+    @subject.get(:image).should.be.kind_of(UIImageView)
+  end
+
+end
