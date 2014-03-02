22d84f9deea841ed59fc430e613ec93f50446883
Colin T.A. Gray <colinta@gmail.com>
2014-03-01 17:37:56 -0700

    added 'motion_kit_meta', to hold parent and delegate

--- lib/motion-kit/object.rb
+++ lib/motion-kit/object.rb
@@ -1,3 +1,16 @@
 class Object
-  attr_accessor :motion_kit_id
+  attr_accessor :motion_kit_meta
+
+  def motion_kit_id=(element_id)
+    motion_kit_meta[:id] = element_id
+  end
+
+  def motion_kit_id
+    motion_kit_meta[:id]
+  end
+
+  def motion_kit_meta
+    @motion_kit_meta ||= {}
+  end
+
 end
