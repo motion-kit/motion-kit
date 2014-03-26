b05d20bc6ce5d8001045f43bad917a5809a43fc2
Jamon Holmgren <jamon@clearsightstudio.com>
2014-03-26 09:41:03 -0700

    Refactored base_layout quite a bit, added motion-redgreen for better test output, moved class methods for base_layout into its own file to reduce massiveness

--- lib/motion-kit/layouts/base_layout.rb
+++ lib/motion-kit/layouts/base_layout.rb
@@ -1,3 +1,5 @@
+motion_require "./base_layout_class_methods"
+
 module MotionKit
   # Abstract base class, responsible for "registration" of layout classes with
   # the class `targets` method.
@@ -9,79 +11,17 @@ module MotionKit
   # The ViewLayout subclass defines methods that are appropriate for adding and
   # removing views to a view hierarchy.
   class BaseLayout
-
-    class << self
-
-      def target_klasses
-        # We only want to return the *BaseLayout*'s target_klasses store, not
-        # a subclass'.
-        return BaseLayout.target_klasses unless self == BaseLayout
-        @target_klasses ||= {}
-      end
-
-      def targets(klass=nil)
-        if klass.nil?
-          if self == BaseLayout
-            nil
-          else
-            @targets || superclass.targets
-          end
-        else
-          @targets = klass
-          BaseLayout.target_klasses[klass] = self
-          nil
-        end
-      end
-
-      # Instantiates a new Layout instance using `layout` as the root-level layout
-      def layout_for(layout, klass)
-        target_klasses = BaseLayout.target_klasses
-
-        # shave a little bit of time by caching the result of this klass/layout
-        # lookup.
-        @memoize ||= {}
-        unless @memoize[klass]
-          # go up the classes ancestors looking for a registered class. This is
-          # how new subclasses can inherit an existing registered Layout from
-          # one of its superclasses.
-          while klass
-            if registered_class = target_klasses[klass]
-              break
-            end
-            klass = klass.superclass
-          end
-          return nil unless registered_class
-
-          @memoize[klass] = registered_class
-        end
-
-        return @memoize[klass].new_child(layout)
-      end
-
-      def new_child(layout=nil)
-        child = self.new
-        child.set_layout(layout)
-        return child
-      end
-
-    end
+    # Class methods reside in base_layout_class_methods.rb
+    extend BaseLayoutClassMethods
 
     attr :parent
 
     def initialize
-      # if you're tempted to set @layout_delegate here - don't. In a ViewLayout,
-      # we could instantiate a 'root' view that does *not* use the same Layout
-      # as the current class. Leave the delegate as 'nil' so it can be created
-      # in 'context'.
-
-      # the object to look in for style methods
+      # @layout is the object we look in for style methods
       @layout = self
-      # the Layout object that implements custom style methods
+      # the Layout object that implements custom style methods. Leave this as nil
+      # in the initializer.
       @layout_delegate = nil
-      # this variable is almost always 'initial'. It is only set to :reapply
-      # from the reapply! method. Makes sense when you consider the fact that
-      # you can have arbitrary view-creation methods (other than 'layout'), and
-      # they would be expected to support `initial/reapply` blocks as well.
       @layout_state = :initial
     end
 
@@ -133,20 +73,16 @@ module MotionKit
       # the top-level Layout object.
       return @layout.context(target, &block) if @layout && @layout != self
 
-      context_was = @context
-      parent_was = @parent
-      delegate_was = @layout_delegate
+      context_was, parent_was, delegate_was = @context, @parent, @layout_delegate
 
       @parent = MK::Parent.new(context_was)
       @context = target
       @context.motion_kit_meta[:delegate] ||= Layout.layout_for(@layout, @context.class)
       @layout_delegate = @context.motion_kit_meta[:delegate]
       yield
-      @layout_delegate = delegate_was
-      @context = context_was
-      @parent = parent_was
+      @layout_delegate, @context, @parent = delegate_was, context_was, parent_was
 
-      return target
+      target
     end
 
     # All missing methods get delegated to either the @layout_delegate or
@@ -167,57 +103,46 @@ module MotionKit
       method_name = method_name.to_s
       raise ApplyError.new("Cannot apply #{method_name.inspect} to instance of #{target.class.name}") if method_name.length == 0
 
-      target = self.target
       @layout_delegate ||= Layout.layout_for(@layout, target.class)
-      if @layout_delegate.respond_to?(method_name)
-        return @layout_delegate.send(method_name, *args, &block)
-      end
-
+      return @layout_delegate.send(method_name, *args, &block) if @layout_delegate.respond_to?(method_name)
+      
       if block
-        if target.respondsToSelector(method_name)
-          new_context = target.send(method_name, *args)
-          return self.context(new_context, &block)
-        elsif method_name.include?('_')
-          objc_name = MotionKit.objective_c_method_name(method_name)
-          return self.apply(objc_name, *args, &block)
-        else
-          raise ApplyError.new("Cannot apply #{method_name.inspect} to instance of #{target.class.name}")
-        end
-      end
-
-      # setters are not touched; just send the method as-is, and don't bother
-      # with checking `method_name=`.
-      if method_name =~ /^set[A-Z]/
-        assign = nil
-        setter = method_name + ':'
+        apply_with_context(method_name, *args, &block)
       else
-        assign = method_name + '='
-        setter = 'set' + method_name[0].capitalize + method_name[1..-1] + ':'
+        apply_with_target(method_name, *args, &block)
       end
+    end
 
-      # the order of checking methods is important; Ruby classes can return
-      # 'true' for `respond_to?('foo=')` even though `send('foo=')` will raise a
-      # NoMethodError.
+    def apply_with_context(method_name, *args, &block)
+      if target.respondsToSelector(method_name)
+        new_context = target.send(method_name, *args)
+        self.context(new_context, &block)
+      elsif method_name.include?('_')
+        objc_name = MotionKit.objective_c_method_name(method_name)
+        self.apply(objc_name, *args, &block)
+      else
+        raise ApplyError.new("Cannot apply #{method_name.inspect} to instance of #{target.class.name}")
+      end
+    end
 
-      # objc classes are the opposite
-      if target.respondsToSelector(setter)
-        return target.send(setter, *args)
-      # ruby classes will probably define the attr_accessor, not a 'setFoo' method
-      elsif assign && target.respond_to?(assign)
-        return target.send(assign, *args)
-      # it's possible this is a setter of the form `foo(value)`. We only call
-      # this method if the arity is NOT 0, so getters are always avoided.
-      # Unless args is empty, in which case we WANT to call the getter
-      elsif target.respond_to?(method_name) && (target.method(method_name).arity != 0 || args.empty?)
-        return target.send(method_name, *args)
+    def apply_with_target(method_name, *args, &block)
+      setter = "set" + method_name.capitalize + ':'
+      assign = method_name + '='
+      
+      # The order is important here.
+      if target.respond_to?(setter)
+        target.send(setter, *args)
+      elsif target.respond_to?(assign)
+        target.send(assign, *args)
+      elsif target.respond_to?(method_name)
+        target.send(method_name, *args)
       # UIAppearance classes are a whole OTHER thing; they never return 'true'
       elsif target.is_a?(MotionKit.appearance_class)
-        return target.send(setter, *args)
+        target.send(setter, *args)
       # Finally, try again with camel case if there's an underscore.
       elsif method_name.include?('_')
         objc_name = MotionKit.objective_c_method_name(method_name)
-        return self.apply(objc_name, *args)
-      # failure
+        self.apply(objc_name, *args)
       else
         raise ApplyError.new("Cannot apply #{method_name.inspect} to instance of #{target.class.name}")
       end
