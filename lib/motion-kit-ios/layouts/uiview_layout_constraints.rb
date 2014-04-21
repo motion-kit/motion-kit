00ca236b96154955be6596bd1d72bd2acaf9b1da
Colin T.A. Gray <colinta@gmail.com>
2014-04-21 14:48:39 -0600

    constraints files

---
+++ lib/motion-kit-ios/layouts/uiview_layout_constraints.rb
@@ -0,0 +1,10 @@
+motion_require 'uiview_layout'
+
+module MotionKit
+  class UIViewLayout
+
+    def constraints(&block)
+    end
+
+  end
+end
---
+++ lib/motion-kit-osx/layouts/nsview_layout_constraints.rb
@@ -0,0 +1,10 @@
+motion_require 'nsview_layout'
+
+module MotionKit
+  class NSViewLayout
+
+    def constraints(&block)
+    end
+
+  end
+end
