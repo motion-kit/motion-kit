b1c891fe2aae1bcd9532e0127658759c1c3ac729
Colin T.A. Gray <colinta@gmail.com>
2014-05-02 16:11:38 -0600

    convert case

--- samples/ios/AutoLayout/app/app_delegate.rb
+++ samples/ios/AutoLayout/app/app_delegate.rb
@@ -1,9 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    myNavController = RootController.alloc.init
+    root_controller = RootController.alloc.init
 
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(myNavController)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(root_controller)
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
 
