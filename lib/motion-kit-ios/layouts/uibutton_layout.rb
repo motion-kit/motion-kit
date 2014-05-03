35ec81a15db6b1ea884133fa62333d9d0eafac10
Colin T.A. Gray <colinta@gmail.com>
2014-05-02 18:12:05 -0600

    UIButton helpers

--- lib/motion-kit-ios/layouts/uibutton_layout.rb
+++ lib/motion-kit-ios/layouts/uibutton_layout.rb
@@ -5,7 +5,23 @@ module MotionKit
     targets UIButton
 
     def title(value)
-      target.setTitle(value, forState: UIControlStateNormal)
+      if value.is_a?(NSAttributedString)
+        target.setAttributedTitle(value, forState: UIControlStateNormal)
+      else
+        target.setTitle(value, forState: UIControlStateNormal)
+      end
+    end
+
+    def title_color(value)
+      target.setTitleColor(value, forState: UIControlStateNormal)
+    end
+
+    def title_shadow_color(value)
+      target.setTitleShadowColor(value, forState: UIControlStateNormal)
+    end
+
+    def background_image(value)
+      target.setBackgroundImage(value, forState: UIControlStateNormal)
     end
 
     def image(value)
