29513f54b51f008610473d68101f24d90a60c89c
Colin T.A. Gray <colinta@gmail.com>
2014-05-07 09:38:48 -0600

    bump version, finished gradient layer fixes

--- lib/motion-kit-ios/layouts/uiview_layout_gradient.rb
+++ lib/motion-kit-ios/layouts/uiview_layout_gradient.rb
@@ -8,9 +8,11 @@ module MotionKit
         gradient_layer = CAGradientLayer.layer
         gradient_layer.frame = CGRect.new([0, 0], target.frame.size)
         target.layer.insertSublayer(gradient_layer, atIndex:0)
+        target.motion_kit_meta[:motionkit_gradient_layer] = gradient_layer
 
         gradient_layer
       end
+
       context(gradient_layer, &block)
 
       gradient_layer
