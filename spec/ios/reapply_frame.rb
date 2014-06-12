e1bf97b3587f93fe185de8c24758eeb8c7c552a2
Johannes Haubold <jh@megorei.com>
2014-06-12 16:42:54 +0200

    fixed test

--- spec/ios/reapply_frame.rb
+++ spec/ios/reapply_frame.rb
@@ -2,73 +2,64 @@ describe 'Views frames should remain unchanged' do
 
   before do
     @subject = ReapplyTestLayout.new
+    @cw = @subject.view.frame.size.width  # container width
+    @ch = @subject.view.frame.size.height # container height
   end
 
-  it "top_left should be in the right position before reapply" do
+  it "top_left should be in the right position before and after reapply" do
     @subject.get(:top_left).frame.should == CGRectMake(0,0,10,10)
-  end
-
-  it "top should be in the right position before reapply" do
-    @subject.get(:top).frame.should == CGRectMake(45,0,10,10)
-  end
-  it "top should be in the right position before reapply" do
-    @subject.get(:top_right).frame.should == CGRectMake(90,0,10,10)
-  end
-  it "top should be in the right position before reapply" do
-    @subject.get(:left).frame.should == CGRectMake(0,45,10,10)
-  end
-  it "top should be in the right position before reapply" do
-    @subject.get(:center).frame.should == CGRectMake(45,45,10,10)
-  end
-  it "top should be in the right position before reapply" do
-    @subject.get(:right).frame.should == CGRectMake(90,45,10,10)
-  end
-  it "top should be in the right position before reapply" do
-    @subject.get(:bottom_left).frame.should == CGRectMake(0,90,10,10)
-  end
-
-  it "top should be in the right position before reapply" do
-    @subject.get(:bottom).frame.should == CGRectMake(45,90,10,10)
-  end
-
-  it "top should be in the right position before reapply" do
-    @subject.get(:bottom_right).frame.should == CGRectMake(90,90,10,10)
-  end
-
-  it "reapplies the frames" do
     @subject.reapply!
-    1.should == 1
-  end
-
-  it "top_left should be in the right position after reapply" do
     @subject.get(:top_left).frame.should == CGRectMake(0,0,10,10)
   end
 
-  it "top should be in the right position after reapply" do
-    @subject.get(:top).frame.should == CGRectMake(45,0,10,10)
-  end
-  it "top should be in the right position after reapply" do
-    @subject.get(:top_right).frame.should == CGRectMake(90,0,10,10)
-  end
-  it "top should be in the right position after reapply" do
-    @subject.get(:left).frame.should == CGRectMake(0,45,10,10)
-  end
-  it "top should be in the right position after reapply" do
-    @subject.get(:center).frame.should == CGRectMake(45,45,10,10)
-  end
-  it "top should be in the right position after reapply" do
-    @subject.get(:right).frame.should == CGRectMake(90,45,10,10)
-  end
-  it "top should be in the right position after reapply" do
-    @subject.get(:bottom_left).frame.should == CGRectMake(0,90,10,10)
+  it "top should be in the right position before and after reapply" do
+    NSLog("top frame before reapply = #{@subject.get(:top).frame.inspect}")
+    @subject.get(:top).frame.should == CGRectMake(@cw/2 - 5, 0, 10, 10)
+    @subject.reapply!
+    NSLog("top frame after reapply = #{@subject.get(:top).frame.inspect}")
+    @subject.get(:top).frame.should == CGRectMake(@cw/2 - 5, 0, 10, 10)
   end
 
-  it "top should be in the right position after reapply" do
-    @subject.get(:bottom).frame.should == CGRectMake(45,90,10,10)
+  it "top_right should be in the right position before and after reapply" do
+    @subject.get(:top_right).frame.should == CGRectMake(@cw - 10, 0, 10, 10)
+    @subject.reapply!
+    @subject.get(:top_right).frame.should == CGRectMake(@cw - 10, 0, 10, 10)
   end
 
-  it "top should be in the right position after reapply" do
-    @subject.get(:bottom_right).frame.should == CGRectMake(90,90,10,10)
+  it "left should be in the right position before and after reapply" do
+    @subject.get(:left).frame.should == CGRectMake(0, @ch/2 - 5, 10, 10)
+    @subject.reapply!
+    @subject.get(:left).frame.should == CGRectMake(0, @ch/2 - 5, 10, 10)
+  end
+
+  it "center should be in the right position before and after reapply" do
+    @subject.get(:center).frame.should == CGRectMake(@cw/2 - 5, @ch/2 - 5, 10, 10)
+    @subject.reapply!
+    @subject.get(:center).frame.should == CGRectMake(@cw/2 - 5, @ch/2 - 5, 10, 10)
+  end
+
+  it "right should be in the right position before and after reapply" do
+    @subject.get(:right).frame.should == CGRectMake(@cw - 10, @ch/2 - 5, 10, 10)
+    @subject.reapply!
+    @subject.get(:right).frame.should == CGRectMake(@cw - 10, @ch/2 - 5, 10, 10)
+  end
+
+  it "bottom_left should be in the right position before and after reapply" do
+    @subject.get(:bottom_left).frame.should == CGRectMake(0, @ch - 10, 10, 10)
+    @subject.reapply!
+    @subject.get(:bottom_left).frame.should == CGRectMake(0, @ch - 10, 10, 10)
+  end
+
+  it "bottom should be in the right position before and after reapply" do
+    @subject.get(:bottom).frame.should == CGRectMake(@cw/2 - 5, @ch - 10, 10, 10)
+    @subject.reapply!
+    @subject.get(:bottom).frame.should == CGRectMake(@cw/2 - 5, @ch - 10, 10, 10)
+  end
+
+  it "bottom_right should be in the right position before and after reapply" do
+    @subject.get(:bottom_right).frame.should == CGRectMake(@cw - 10, @ch - 10, 10, 10)
+    @subject.reapply!
+    @subject.get(:bottom_right).frame.should == CGRectMake(@cw - 10, @ch - 10, 10, 10)
   end
 
 end
