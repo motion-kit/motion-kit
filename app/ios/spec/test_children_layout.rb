7619aa94a7d638c615df42640cdb9d8c7ef298c0
Colin T.A. Gray <colinta@gmail.com>
2014-07-09 13:15:09 -0600

    update child layout specs to test for reapply

--- app/ios/spec/test_children_layout.rb
+++ app/ios/spec/test_children_layout.rb
@@ -1,9 +1,19 @@
 class TestChildrenLayout < MK::Layout
 
   def layout
+    add UILabel, :label
     add ChildLabelLayout
     add ChildButtonLayout, :child_button
     add ChildImageLayout, :child_image
+  end
+
+  def label_style
+    initial do
+      text 'root foo'
+    end
+    reapply do
+      text 'root reapplied'
+    end
   end
 
 end
@@ -12,7 +22,16 @@ end
 class ChildLabelLayout < MK::Layout
 
   def layout
-    root UILabel
+    root UILabel, :label
+  end
+
+  def label_style
+    initial do
+      text 'foo'
+    end
+    reapply do
+      text 'reapplied'
+    end
   end
 
 end
@@ -21,7 +40,16 @@ end
 class ChildButtonLayout < MK::Layout
 
   def layout
-    root UIButton
+    root UIButton, :button
+  end
+
+  def button_style
+    initial do
+      title 'foo'
+    end
+    reapply do
+      title 'reapplied'
+    end
   end
 
 end
@@ -30,7 +58,34 @@ end
 class ChildImageLayout < MK::Layout
 
   def layout
-    root UIImageView
+    root UIImageView, :image
+  end
+
+  def blank_image
+    @blank_image ||= begin
+      UIGraphicsBeginImageContextWithOptions([10, 10], false, UIScreen.mainScreen.scale)
+      blank_image = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+      blank_image
+    end
+  end
+
+  def reapplied_image
+    @reapplied_image ||= begin
+      UIGraphicsBeginImageContextWithOptions([11, 11], false, UIScreen.mainScreen.scale)
+      reapplied_image = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+      reapplied_image
+    end
+  end
+
+  def image_style
+    initial do
+      image blank_image
+    end
+    reapply do
+      image reapplied_image
+    end
   end
 
 end
