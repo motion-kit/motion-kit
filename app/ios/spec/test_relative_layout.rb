322c137ff71b25fb7beceda69a5d7f6c975aa62c
Colin T.A. Gray <colinta@gmail.com>
2014-09-04 12:02:13 -0600

    I don't know why i'm so particular about quotes... it's a compulsion

--- app/ios/spec/test_relative_layout.rb
+++ app/ios/spec/test_relative_layout.rb
@@ -2,12 +2,12 @@ class TestRelativeLayout < MotionKit::Layout
 
   def layout
     add UIView, :test do
-      frame from_top_left(w: "100%", h: 100)
+      frame from_top_left(w: '100%', h: 100)
       background_color UIColor.redColor
     end
 
     add UIView, :test2 do
-      frame below(:test, w: "100%", h: 50)
+      frame below(:test, w: '100%', h: 50)
       background_color UIColor.greenColor
     end
   end
