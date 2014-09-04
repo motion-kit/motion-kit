dcb391ee90b50eba96ffb00f789bbeef179a79eb
Colin T.A. Gray <colinta@gmail.com>
2014-09-04 14:47:17 -0600

    oh yeah, parent classes

--- lib/motion-kit-osx/deprecated.rb
+++ lib/motion-kit-osx/deprecated.rb
@@ -1,42 +1,49 @@
+# @requires MotionKit::NSWindowHelpers
+# @requires MotionKit::NSViewHelpers
+# @requires MotionKit::NSTableViewHelpers
+# @requires MotionKit::NSTableColumnHelpers
+# @requires MotionKit::NSMenuHelpers
+# @requires MotionKit::CALayerHelpers
+# @requires MotionKit::CAGradientLayerHelpers
 module MotionKit
 
-  class NSWindowLayout
+  class NSWindowLayout < NSWindowHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  NSWindowLayout is now NSWindowHelpers.  Update #{subclass} to extend from NSWindowHelpers.")
     end
   end
 
-  class NSViewLayout
+  class NSViewLayout < NSViewHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  NSViewLayout is now NSViewHelpers.  Update #{subclass} to extend from NSViewHelpers.")
     end
   end
 
-  class NSTableViewLayout
+  class NSTableViewLayout < NSTableViewHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  NSTableViewLayout is now NSTableViewHelpers.  Update #{subclass} to extend from NSTableViewHelpers.")
     end
   end
 
-  class NSTableColumnLayout
+  class NSTableColumnLayout < NSTableColumnHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  NSTableColumnLayout is now NSTableColumnHelpers.  Update #{subclass} to extend from NSTableColumnHelpers.")
     end
   end
 
-  class NSMenuLayout
+  class NSMenuLayout < NSMenuHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  NSMenuLayout is now NSMenuHelpers.  Update #{subclass} to extend from NSMenuHelpers.")
     end
   end
 
-  class CALayerLayout
+  class CALayerLayout < CALayerHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  CALayerLayout is now CALayerHelpers.  Update #{subclass} to extend from CALayerHelpers.")
     end
   end
 
-  class CAGradientLayerLayout
+  class CAGradientLayerLayout < CAGradientLayerHelpers
     def self.inherited(subclass)
       NSLog("Sorry!  MotionKit changed.  CAGradientLayerLayout is now CAGradientLayerHelpers.  Update #{subclass} to extend from CAGradientLayerHelpers.")
     end
