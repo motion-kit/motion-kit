4ffbd7deb0665ea1ecadb327c7c08699337e5979
David Hersey <david.hersey@gmail.com>
2016-07-22 13:18:02 -0400

    increased recursion to all levels of subviews

--- lib/motion-kit/helpers/tree_layout.rb
+++ lib/motion-kit/helpers/tree_layout.rb
@@ -463,21 +463,24 @@ module MotionKit
       unless is_parent_layout?
         return parent_layout.remove_view(element_id, view)
       end
-      mp "forgetting #{element_id}, #{view}"
+      # mp "forgetting #{element_id}, #{view}"
       removed = nil
       context(self.view) do
-        removed = @elements[element_id].delete(view)
+        removed = @elements[element_id].delete(view) if @elements[element_id]
       end
       removed
     end
 
     # returns the root view that was removed, if any
-    def forget_tree(element_id, view)  
-      removed = forget_view(element_id, view) 
-      view.subviews.each do | sub |
-        sub_ids = sub.motion_kit_meta[:motion_kit_ids]
-        sub_ids.each do | sub_id |
-          forget_view(sub_id, sub) || []
+    def forget_tree(element_id, view)
+      removed = forget_view(element_id, view)
+      if view.subviews
+        view.subviews.each do | sub |
+          if (sub_ids = sub.motion_kit_meta[:motion_kit_ids])
+            sub_ids.each do | sub_id |
+              forget_tree(sub_id, sub) || []
+            end
+          end
         end
       end
       removed
