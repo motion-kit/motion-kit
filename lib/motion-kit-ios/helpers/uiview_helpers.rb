1417b0ad34e88ee7a2af0139a42925348e630ffc
David Hersey <david.hersey@gmail.com>
2016-10-20 11:51:59 +0300

    supporting behind: and in_front_of: options for adding subviews

--- lib/motion-kit-ios/helpers/uiview_helpers.rb
+++ lib/motion-kit-ios/helpers/uiview_helpers.rb
@@ -11,8 +11,16 @@ module MotionKit
       view_class.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     end
 
-    def add_child(subview)
-      target.addSubview(subview)
+    def add_child(subview, options={})
+      if (sibling = options[:behind])
+        target.insertSubview(subview, belowSubview: sibling)
+      elsif (sibling = options[:in_front_of])
+        target.insertSubview(subview, aboveSubview: sibling)
+      elsif (z_index = options[:z_index])
+        target.insertSubview(subview, atIndex: z_index)
+      else
+        target.addSubview(subview)
+      end
     end
 
     def remove_child(subview)
